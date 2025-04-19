33de0cd3b6880b96dcd054da8bcd5a1679ad7511
Eduardo Sacahui <eduardo.sacahui@gmail.com>
2025-04-19 23:17:06 +0000

    style(ui): add neon border to card
    Original: wao it looks great but a bit plain, I want the border of the card to be a of a raingbow neon color,

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,6 +3,21 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+const CardNeon = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-lg border bg-card text-card-foreground shadow-sm border-double border-width-2 border-image-slice-1 border-image-source:linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)",
+      className
+    )}
+    {...props}
+  />
+))
+CardNeon.displayName = "CardNeon"
+
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -77,4 +92,7 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { CardNeon as Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+
+
+    
