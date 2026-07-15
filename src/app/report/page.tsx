48e1f449cba397c532ae00c6241500527cd9603e
BernydotJar <fearlesed@gmail.com>
2026-07-15 01:23:45 -0600

    feat: add dedicated report route

---
+++ src/app/report/page.tsx
@@ -0,0 +1,8 @@
+export const dynamic = "force-static";
+
+import ReportPageClient from "./ReportPageClient";
+import "./report.css";
+
+export default function ReportPage() {
+  return <ReportPageClient />;
+}
